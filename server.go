79bebca012ffd762e1678cead1a9520d86be54b5
taoshihan1991 <630892807@qq.com>
2020-05-21 19:41:48 +0800

    优化邮件夹列表展示和跳转到邮件详情

--- server.go
+++ server.go
@@ -26,6 +26,8 @@ func main() {
 	http.HandleFunc("/check", check)
 	//邮件夹接口
 	http.HandleFunc("/folders", folders)
+	//邮件接口
+	http.HandleFunc("/mail", mail)
 	//详情界面
 	http.HandleFunc("/view", view)
 	//监听端口
@@ -53,71 +55,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 //输出列表
 func list(w http.ResponseWriter, r *http.Request) {
-	values := r.URL.Query()
-	fid := ""
-	currentPage := 0
-	if len(values["fid"]) != 0 {
-		fid = values["fid"][0]
-	}
-	if len(values["page"]) != 0 {
-		currentPage, _ = strconv.Atoi(values["page"][0])
-	}
-	if fid == "" {
-		fid = "INBOX"
-	}
-	if currentPage == 0 {
-		currentPage = 1
-	}
-
-	mailServer:=tools.GetMailServerFromCookie(r)
-
-	render := new(tools.IndexData)
-	render.CurrentPage = currentPage
-	var prePage int
-	if (currentPage - 1) <= 0 {
-		prePage = 1
-	} else {
-		prePage = currentPage - 1
-	}
-	render.PrePage = fmt.Sprintf("/list?fid=%s&page=%d", fid, prePage)
-	render.NextPage = fmt.Sprintf("/list?fid=%s&page=%d", fid, currentPage+1)
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		folders := tools.GetFolders(mailServer.Server, mailServer.Email,mailServer.Password, fid)
-		render.Folders = folders
-		render.Fid = fid
-
-		//PageCount:= render.Folders[fid]/PAGE_SIZE
-		numPages := ""
-		start := currentPage - 5
-		if start <= 0 {
-			start = 1
-		}
-		end := start + 11
-		//if end>=PageCount{
-		//	end=PageCount
-		//}
-
-		for i := start; i < end; i++ {
-			active := ""
-			if currentPage == i {
-				active = "active"
-			}
-			numPages += fmt.Sprintf("<li class=\"page-item %s\"><a class=\"page-link\" href=\"/list?fid=%s&page=%d\">%d</a></li>", active, fid, i, i)
-		}
-		render.NumPages = template.HTML(numPages)
-	}()
-	go func() {
-		defer wg.Done()
-		mails := tools.GetFolderMail(mailServer.Server, mailServer.Email, mailServer.Password, fid, currentPage, PageSize)
-		render.MailPagelist = mails
-	}()
-
-	wg.Wait()
-	tmpl.RenderList(w, render)
+	tmpl.RenderList(w, nil)
 }
 
 //详情界面
@@ -249,6 +187,8 @@ func folders(w http.ResponseWriter, r *http.Request) {
 		result["mails"]=mails
 	}()
 	wg.Wait()
+	result["pagesize"]=PageSize
+	result["fid"]=fid
 
 	msg, _ := json.Marshal(tools.JsonFolders{
 		JsonResult: tools.JsonResult{Code: 200, Msg: "获取成功"},
@@ -256,8 +196,55 @@ func folders(w http.ResponseWriter, r *http.Request) {
 	})
 	w.Write(msg)
 }
-//加密cookie
-//func authCookie(){
-//
-//}
+//邮件接口
+func mail(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	fid := ""
+	if len(values["fid"]) != 0 {
+		fid = values["fid"][0]
+	} else {
+		fid = "INBOX"
+	}
+	var id uint32
+	if len(values["id"]) != 0 {
+		i, _ := strconv.Atoi(values["id"][0])
+		id = uint32(i)
+	} else {
+		id = 0
+	}
+	mailServer:=tools.GetMailServerFromCookie(r)
+	w.Header().Set("content-type","text/json;charset=utf-8;")
+
+	if mailServer==nil{
+		msg, _ := json.Marshal(tools.JsonResult{Code: 400, Msg: "验证失败"})
+		w.Write(msg)
+		return
+	}
+	var wg sync.WaitGroup
+	result :=make(map[string]interface{})
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		folders := tools.GetFolders(mailServer.Server, mailServer.Email, mailServer.Password, fid)
+		result["folders"]=folders
+		result["total"]=folders[fid]
+	}()
+	go func() {
+		defer wg.Done()
+		mail := tools.GetMessage(mailServer.Server, mailServer.Email, mailServer.Password, fid, id)
+		result["from"] = mail.From
+		result["to"] = mail.To
+		result["subject"] = mail.Subject
+		result["date"] = mail.Date
+		result["html"] = mail.Body
+	}()
+	wg.Wait()
+	result["fid"]=fid
+
+	msg, _ := json.Marshal(tools.JsonFolders{
+		JsonResult: tools.JsonResult{Code: 200, Msg: "获取成功"},
+		Result:     result,
+	})
+	w.Write(msg)
+}
 
