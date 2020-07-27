41be88ad78f08a445d03aa5bd2ebaaf4bad3d1d6
taoshihan1991 <630892807@qq.com>
2020-07-27 15:56:34 +0800

    增加github

--- static/js/functions.js
+++ static/js/functions.js
@@ -18,3 +18,23 @@ function getWsBaseUrl() {
     }
     return url;
 }
+function notify(title, options, callback) {
+    // 先检查浏览器是否支持
+    if (!window.Notification) {
+        return;
+    }
+    var notification;
+    // 检查用户曾经是否同意接受通知
+    if (Notification.permission === 'granted') {
+        notification = new Notification(title, options); // 显示通知
+
+    } else {
+        var promise = Notification.requestPermission();
+    }
+
+    if (notification && callback) {
+        notification.onclick = function(event) {
+            callback(notification, event);
+        }
+    }
+}
