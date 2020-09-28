5c6727e643a37069a801b69798eaf991a1abdaee
taoshihan1991 <630892807@qq.com>
2020-09-28 11:09:09 +0800

    首页增加js变量

--- static/js/gofly-front.js
+++ static/js/gofly-front.js
@@ -8,13 +8,13 @@ $("#launchButton").click(function() {
     }
     layer.open({
         type: 2,
-        title: "Chat with us",
+        title: "Chat with me",
         closeBtn: 1, //不显示关闭按钮
         shade: [0],
         area: ['520px', '530px'],
         offset: 'rb', //右下角弹出
         anim: 2,
-        content: [GOFLY_URL+'/chatIndex?refer='+window.document.title, 'yes'], //iframe的url，no代表不显示滚动条
+        content: [GOFLY_URL+'/chatIndex?kefu_id='+GOFLY_KEFU_ID+'&refer='+window.document.title, 'yes'], //iframe的url，no代表不显示滚动条
         end: function(){
             launchButtonFlag=false;
             $(".launchButton").show();
