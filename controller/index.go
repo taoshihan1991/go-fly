4432cb7012c767d58e403d0e2b98c4922adc58de
陶士涵 <630892807@qq.com>
2020-10-16 21:16:25 +0800

    首页跳转语言可配置

--- controller/index.go
+++ controller/index.go
@@ -2,29 +2,13 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/taoshihan1991/imaptool/tools"
-	"net/http"
+	"github.com/taoshihan1991/imaptool/models"
 )
 
 func Index(c *gin.Context) {
-	c.Redirect(302, "/index_en")
-}
-
-//首页跳转
-func ActionIndex(w http.ResponseWriter, r *http.Request) {
-	if r.URL.RequestURI() == "/favicon.ico" {
-		return
+	jump := models.FindConfig("JumpLang")
+	if jump != "cn" {
+		jump = "en"
 	}
-
-	mailServer := tools.GetMailServerFromCookie(r)
-	if mailServer == nil {
-		http.Redirect(w, r, "/login", 302)
-	} else {
-		res := tools.CheckEmailPassword(mailServer.Server, mailServer.Email, mailServer.Password)
-		if res {
-			http.Redirect(w, r, "/main", 302)
-		} else {
-			http.Redirect(w, r, "/login", 302)
-		}
-	}
+	c.Redirect(302, "/index_"+jump)
 }
