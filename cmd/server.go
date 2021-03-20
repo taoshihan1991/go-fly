038b53b9747dde502930c8f350baa6d4ab0c1b34
630892807@qq.com <630892807@qq.com>
2021-03-20 13:05:13 +0800

    更新守护进程

--- cmd/server.go
+++ cmd/server.go
@@ -45,17 +45,6 @@ func run() {
 		d := xdaemon.NewDaemon(logFilePath + "go-fly.log")
 		d.MaxCount = 5
 		d.Run()
-		//if os.Getppid() != 1 {
-		//	// 将命令行参数中执行文件路径转换成可用路径
-		//	filePath, _ := filepath.Abs(os.Args[0])
-		//	cmd := exec.Command(filePath, os.Args[1:]...)
-		//	// 将其他命令传入生成出的进程
-		//	cmd.Stdin = os.Stdin // 给新进程设置文件描述符，可以重定向到文件中
-		//	cmd.Stdout = os.Stdout
-		//	cmd.Stderr = os.Stderr
-		//	cmd.Start() // 开始执行新进程，不等待新进程退出
-		//	os.Exit(0)
-		//}
 	}
 
 	baseServer := "0.0.0.0:" + Port
