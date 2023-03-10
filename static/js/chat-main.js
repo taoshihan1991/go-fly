dc573856d1d96d925391a850364c697dc9aab3e4
taoshihan <630892807@qq.com>
2023-03-10 12:49:07 +0800

    后台界面样式调整

--- static/js/chat-main.js
+++ static/js/chat-main.js
@@ -547,13 +547,7 @@ var app=new Vue({
 
                 });
             });
-            var _hmt = _hmt || [];
-            (function() {
-                var hm = document.createElement("script");
-                hm.src = "https://hm.baidu.com/hm.js?82938760e00806c6c57adee91f39aa5e";
-                var s = document.getElementsByTagName("script")[0];
-                s.parentNode.insertBefore(hm, s);
-            })();
+
         },
         //表情点击事件
         faceIconClick(index){
